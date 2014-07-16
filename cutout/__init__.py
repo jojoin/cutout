3d82732f8a316d8807795d16e89a6893a8d09e41
杨捷 <myworld4059@163.com>
2014-07-16 14:59:34 +0800

    Update __init__.py

--- cutout/__init__.py
+++ cutout/__init__.py
@@ -3,14 +3,20 @@
 
 
 
+
+import os.path
+import sys
+import urllib
+import urllib.request
+import urllib.parse
+import time
 import re
-from .common import get_html
+
+from .util import sec2time
+from .common import ProgressBar
 
 
-
-
-
-#“剪”出数据
+##“剪”出数据
 def cutout(
 		url=None,
 		data=None,
@@ -66,7 +72,8 @@ def cutout(
 	else:
 		return _dealwith(cutstr,dealwith)
 
-#cutout递归再处理
+
+## cutout递归再处理
 def _dealwith(data,deal):
 	if not deal:
 		return data
@@ -82,9 +89,118 @@ def _dealwith(data,deal):
 		return cutout(**deal)
 
 
-##过滤HTML中的标签
-#将HTML中标签等信息去掉
-#@param htmlstr HTML字符串.
+
+
+## 获取url文件的大小
+def url_size(url):
+	if isinstance(url, str):
+		headers = urllib.request.urlopen(url).headers
+	else:
+		headers = url
+	if 'Content-Length' in headers:
+		return int(headers['Content-Length'])
+	else:
+		return False
+
+
+def undeflate(s):
+	import zlib
+	return zlib.decompress(s, -zlib.MAX_WBITS)
+
+
+def ungzip(s):
+	from StringIO import StringIO
+	import gzip
+	buffer = StringIO(s)
+	f = gzip.GzipFile(fileobj=buffer)
+	return f.read()
+
+
+def get_response(url):
+	headers = {}
+	headers['User-Agent'] = 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'
+	req = urllib.request.Request(
+		url = url,
+		headers = headers
+	)
+	response = urllib.request.urlopen(req)
+	data = response.read()
+	if response.info().get('Content-Encoding') == 'gzip':
+		data = ungzip(data)
+	elif response.info().get('Content-Encoding') == 'deflate':
+		data = undeflate(data)
+	response.data = data
+	return response
+
+
+
+## 抓取网页html
+def get_html(url, encoding=None):
+	content = get_response(url).data
+	if encoding:
+		content = content.decode(encoding,'ignore')
+	return content
+
+
+
+
+## url文件下载
+# @arrive 数据到达回调
+def url_save(url, filepath, headers={} ,arrive=None):
+	req = urllib.request.Request(
+		url = url,
+		headers = headers
+	)
+	response = urllib.request.urlopen(req)
+	#print(response.headers)
+	file_size = url_size(response.headers)
+	#print(file_size)
+	#assert file_size
+	#调节每次下载读取的buffer大小
+	readS = int(file_size/1024/32)
+	if readS < 32: readS = 32
+	if readS > 1024*4: readS = 1024*4
+	#print(readS)
+	with open(filepath, 'wb') as output:
+		while True:
+			buffer = response.read(1024*readS)
+			if not buffer:
+				break
+			size = len(buffer)
+			output.write(buffer)
+			if arrive:
+				arrive(size,file_size)
+
+
+
+## 从url下载文件
+# @pretend 是否伪装成浏览器
+# @referer 来源链接 用于破解防外链
+# @bar 是否产生显示下载进度条
+def url_download(url, filepath, showBar=False, headers={}, pretend=True, referer=None):
+	if referer:
+		headers['Referer'] = referer
+	if pretend:
+		headers['User-Agent'] = 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'
+	#输出下载进度条
+	arrive = None
+	if showBar==True:
+		bar = ProgressBar(piece_total=1);
+	if showBar and not showBar==True:
+		bar = showBar
+	if bar:
+		def arrive(size,file_size):
+			if not bar.displayed:
+				bar.set(piece_total=file_size)
+			bar.step(size)
+	url_save(url, filepath, headers=headers,arrive=arrive)
+
+
+
+
+## 过滤HTML中的标签
+# 将HTML中标签等信息去掉
+# @param htmlstr HTML字符串.
 def remove_html_tags(htmlstr):
     #先过滤CDATA
     re_cdata = re.compile('//<!\[CDATA\[[^>]*//\]\]>',re.I) #匹配CDATA
@@ -107,15 +223,16 @@ def remove_html_tags(htmlstr):
 
 
 
-##替换常用HTML字符实体.
+## 替换常用HTML字符实体.
 def replace_html_char_entity(htmlstr):
 	pass
 
 
-##恢复常用HTML字符实体.
-#使用正常的字符替换HTML中特殊的字符实体.
-#你可以添加新的实体字符到CHAR_ENTITIES中,处理更多HTML字符实体.
-#@param htmlstr HTML字符串.
+
+## 恢复常用HTML字符实体.
+# 使用正常的字符替换HTML中特殊的字符实体.
+# 你可以添加新的实体字符到CHAR_ENTITIES中,处理更多HTML字符实体.
+# @param htmlstr HTML字符串.
 def recover_html_char_entity(htmlstr):
     CHAR_ENTITIES={
 		'nbsp':' ','160':' ',
@@ -142,12 +259,20 @@ def recover_html_char_entity(htmlstr):
 
 
 if __name__=='__main__':
+	print('###html抓取测试')
 	testurl = "http://www.baidu.com"
 	print(testurl)
 	htmlstr = get_html(testurl,encoding='utf-8')
 	print(htmlstr)
 	string = remove_html_tags(htmlstr)
 	print(string)
+
+	print('###文件下载测试')
+	bar = ProgressBar(piece_total=1);
+	bar.face(ui_leg=30)
+	url_download('http://dlsw.baidu.com/sw-search-sp/soft/3a/12350/QQ6.0.1404885253.exe'
+		,'qq.exe',showBar=bar)
+
 
 
 
