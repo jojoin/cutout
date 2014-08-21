7797352ae628fb57aa16f432a177783a016c52ce
杨捷 <myworld4059@163.com>
2014-08-21 16:27:35 +0800

    Create sys.py

---
+++ cutout/sys.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+#-*- coding:utf8 -*-
+
+
+import os
+
+
+## 递归遍历 获取文件夹大小
+def get_dir_size(dir):  
+	size = 0L
+	for root, dirs, files in os.walk(dir):  
+		size += sum([os.path.getsize(os.path.join(root, name)) for name in files])  
+	return size  
+
+
+
+
