d4fc3405543ed1a2b313ac5f766b8dbdd047c328
杨捷 <myworld4059@163.com>
2014-08-21 16:50:45 +0800

    Update sys.py

--- cutout/sys.py
+++ cutout/sys.py
@@ -6,9 +6,14 @@ import os
 
 
 ## 递归遍历 获取文件夹大小
-def dir_size(dir):  
-	size = 0L
-	for root, dirs, files in os.walk(dir):  
+def dir_size(path):
+	exi = os.path.exists(path)
+	if exi:
+		#print('不存在的目录！')
+		return None
+	size = 0
+	for root, dirs, files in os.walk(path):
+		print(root)
 		size += sum([os.path.getsize(os.path.join(root, name)) for name in files])  
 	return size  
 
